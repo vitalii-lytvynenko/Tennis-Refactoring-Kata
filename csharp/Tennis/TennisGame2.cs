936bbd9e80eefd014e9f7051cd135f0a3f606678
LitvinenkoVitalii <reskator95@gmail.com>
2024-02-07 19:37:34 +0200

    Extract methods and variables, refactor logic

--- csharp/Tennis/TennisGame2.cs
+++ csharp/Tennis/TennisGame2.cs
@@ -7,10 +7,10 @@ namespace Tennis
         private int _player1points;
         private int _player2points;
 
-        private string player1result = "";
-        private string player2result = "";
         private readonly string _player1Name;
         private readonly string _player2Name;
+
+        private static readonly string[] pointNames = { "Love", "Fifteen", "Thirty", "Forty" };
 
         public TennisGame2(string player1Name, string player2Name)
         {
@@ -20,91 +20,43 @@ namespace Tennis
 
         public string GetScore()
         {
-            string score = "";
+            var isDraw = _player1points == _player2points;
 
-            if (_player1points == _player2points && _player1points < 3)
-            {
-                if (_player1points == 0)
-                    score = "Love";
-                if (_player1points == 1)
-                    score = "Fifteen";
-                if (_player1points == 2)
-                    score = "Thirty";
-                score += "-All";
-            }
-            if (_player1points == _player2points && _player1points > 2)
-                score = "Deuce";
+            var IsAdvantageOrWin = _player1points >= 4 || _player2points >= 4;
 
-            if (_player1points > 0 && _player2points == 0)
+            if (isDraw)
             {
-                if (_player1points == 1)
-                    player1result = "Fifteen";
-                if (_player1points == 2)
-                    player1result = "Thirty";
-                if (_player1points == 3)
-                    player1result = "Forty";
-
-                player2result = "Love";
-                score = player1result + "-" + player2result;
+                return _player1points >= 3 ? "Deuce" : $"{GetPointName(_player1points)}-All";
             }
-            if (_player2points > 0 && _player1points == 0)
+            else if (IsAdvantageOrWin)
             {
-                if (_player2points == 1)
-                    player2result = "Fifteen";
-                if (_player2points == 2)
-                    player2result = "Thirty";
-                if (_player2points == 3)
-                    player2result = "Forty";
-
-                player1result = "Love";
-                score = player1result + "-" + player2result;
+                return AdvantageOrWinResult();
             }
-
-            if (_player1points > _player2points && _player1points < 4)
+            else
             {
-                if (_player1points == 2)
-                    player1result = "Thirty";
-                if (_player1points == 3)
-                    player1result = "Forty";
-                if (_player2points == 1)
-                    player2result = "Fifteen";
-                if (_player2points == 2)
-                    player2result = "Thirty";
-                score = player1result + "-" + player2result;
+                return $"{GetPointName(_player1points)}-{GetPointName(_player2points)}";
             }
-            if (_player2points > _player1points && _player2points < 4)
-            {
-                if (_player2points == 2)
-                    player2result = "Thirty";
-                if (_player2points == 3)
-                    player2result = "Forty";
-                if (_player1points == 1)
-                    player1result = "Fifteen";
-                if (_player1points == 2)
-                    player1result = "Thirty";
-                score = player1result + "-" + player2result;
-            }
-
-            if (_player1points > _player2points && _player2points >= 3)
-            {
-                score = $"Advantage {_player1Name}";
-            }
-
-            if (_player2points > _player1points && _player1points >= 3)
-            {
-                score = $"Advantage {_player2Name}";
-            }
-
-            if (_player1points >= 4 && _player2points >= 0 && (_player1points - _player2points) >= 2)
-            {
-                score = $"Win for {_player1Name}";
-            }
-            if (_player2points >= 4 && _player1points >= 0 && (_player2points - _player1points) >= 2)
-            {
-                score = $"Win for {_player2Name}";
-            }
-            return score;
         }
+
+        private string AdvantageOrWinResult()
+        {
+            var difference = _player1points - _player2points;
+            if (difference == 1)
+            {
+                return $"Advantage {_player1Name}";
+            }
+            else if (difference == -1)
+            {
+                return $"Advantage {_player2Name}";
+            }
+            else
+            {
+                return difference >= 2 ? $"Win for {_player1Name}" : $"Win for {_player2Name}";
+            }
+        }
+
+        private static string GetPointName(int point) => point >= 0 && point < pointNames.Length ? pointNames[point] : "";
+        
 
         public void WonPoint(string player)
         {
